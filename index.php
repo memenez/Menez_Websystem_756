89382d6de892cc1deea3c1eef7e6919b1365019e
Michael Menez <michael.menez-22@cpu.edu.ph>
2025-02-04 10:40:52 +0800

    Initial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    
+</head>
+
+<body bgcolor="beige">
+    <center>
+        <h1>Hello World!</h1>
+        
+        <h2>
+        <?php
+        include "functions.php";
+        VarInt();
+
+        VarString();
+
+        VarFloat();
+        SumTwoVal();
+        Operator();
+        
+        ?>
+        </h2>
+    </center>
+</body>
+
+</html>
